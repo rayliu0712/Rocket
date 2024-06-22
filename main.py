4b1f8fb870169f731e2056489e77eeae590b0661
LP-w10 <ray2006950712@gmail.com>
2024-06-22 17:02:44 +0800

    轉移2

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import hashlib
+import os
 import os.path as op
 import sys
-import time
+from typing import Callable
 
-from PyQt6.QtCore import QTimer, QThread, Qt
-from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QApplication, QPushButton, QComboBox, QLabel, QWidget, QHBoxLayout, QVBoxLayout, \
-    QSizePolicy, QSpacerItem, QProgressBar
-from adbutils import adb, AdbDevice, ShellReturn
+from PyQt6.QtGui import QShortcut, QKeySequence
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QSpacerItem, QSizePolicy, QHBoxLayout, \
+    QApplication
+from adbutils import AdbDevice, ShellReturn, adb
 
 
 class Device(AdbDevice):
@@ -25,146 +26,122 @@ class Device(AdbDevice):
     def runas(self, cmd: str) -> __ShResult:
         return self.sh(f'run-as rl.launch {cmd}')
 
-    # def pwd(self, cmd: str = '') -> __ShResult:
-    #     sr = self.sh(f'cd "{sdcard}" && {cmd} {'' if cmd == '' else ';'}pwd')
-    #     lines = sr.output.splitlines()
-    #     lines[-1] = lines[-1].replace('//', '/')
-    #     sr.output = '\n'.join(lines)
-    #     return sr
-
     def exists(self, path: str) -> bool:
         return self.sh(f'[[ -e "{path}" ]]').succeed
 
+    def pwd(self, sdcard: str, cmd: str = '') -> __ShResult:
+        sr = self.sh(f'cd "{sdcard}" && {cmd} {'' if cmd == '' else ';'}pwd')
+        lines = sr.output.splitlines()
+        lines[-1] = lines[-1].replace('//', '/')
+        sr.output = '\n'.join(lines)
+        return sr
 
-class Widget(QWidget):
+
+class U:
+    @staticmethod
+    def sha256(string: str) -> str:
+        return hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
+
+    @staticmethod
+    def safe_name(path: str, checker: Callable[[str], bool], return_full: bool) -> str:
+        pwe, ext = op.splitext(path)
+        n = ''
+        for x in range(1, sys.maxsize):
+            if checker(f'{pwe}{n}{ext}'):
+                n = f' ({x})'
+            else:
+                if return_full:
+                    return f'{pwe}{n}{ext}'
+                else:
+                    return op.basename(f'{pwe}{n}{ext}')
+
+    @staticmethod
+    def local_size(path: str) -> int:
+        if not op.exists(path):
+            return 0
+
+        if not op.isdir(path):
+            return op.getsize(path)
+
+        size = 0
+        for r, _, fs in os.walk(path):
+            size += sum(op.getsize(op.join(r, f)) for f in fs)
+        return size
+
+    @staticmethod
+    def remote_size(d: Device, path: str) -> int:
+        try:
+            # sh always succeed here
+            return int(d.sh("find '%s' -type f -exec du -cb {} + | grep total$ | awk '{print $1}'" % path).output)
+        except ValueError:
+            return 0
+
+    @staticmethod
+    def visual_size(length) -> str:
+        i = 0
+        while length >= 1024:
+            length /= 1024
+            i += 1
+
+        if length - int(length) == 0:
+            length = int(length)
+        else:
+            length = round(length, 2)
+
+        return f'{length}{['B', 'KB', 'MB', 'GB'][i]}'
+
+
+class PushWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.label = QLabel(self)
-        self.vice_btn = QPushButton(self)
-        self.push_btn = QPushButton(self)
-        self.push_list = []
-        self.is_launch = True
-        self.timer = QTimer(self)
-        self.setAcceptDrops(True)
+        self.setWindowTitle('Push')
         self.resize(400, 300)
-        self.home()
+        self.setAcceptDrops(True)
+        QShortcut(QKeySequence('Ctrl+V'), self).activated.connect(lambda: self.paste(QApplication.clipboard()))
 
-    def home(self):
-        combo_box = QComboBox(self)
-        combo_box.addItems([d.prop.name for d in adb.device_list()])
-        combo_box.adjustSize()
+        self.label = QLabel('This\nis\nQLabel', self)
+        self.clear_btn = QPushButton('Clear', self)
+        self.clear_btn.clicked.connect(lambda: self.update_label(True))
+        self.push_btn = QPushButton('Push', self)
+        self.push_btn.clicked.connect(lambda: ...)
+        self.push_list = []
 
-        self.label.setText('Wait for launch\n\nor\n\nDrag/Paste files to push')
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        font = QFont()
-        font.setPointSize(12)
-        self.label.setFont(font)
-
-        self.vice_btn.setText('Shell')
-        self.vice_btn.clicked.connect(lambda: print('clicked'))
-
-        self.push_btn.setText('Push')
-        self.push_btn.clicked.connect(lambda: print('clicked'))
-        self.push_btn.setEnabled(False)
-
-        top_hbox = QHBoxLayout()
-        top_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
-        top_hbox.addWidget(combo_box)
-
-        center_hbox = QHBoxLayout()
-        center_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
-        center_hbox.addWidget(self.label)
-        center_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+        mid_hbox = QHBoxLayout()
+        mid_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+        mid_hbox.addWidget(self.label)
+        mid_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
 
         bottom_hbox = QHBoxLayout()
-        bottom_hbox.addWidget(self.vice_btn)
+        bottom_hbox.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum))
+        bottom_hbox.addWidget(self.clear_btn)
         bottom_hbox.addWidget(self.push_btn)
 
         layout = QVBoxLayout()
-        layout.addLayout(top_hbox)
         layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
-        layout.addLayout(center_hbox)
+        layout.addLayout(mid_hbox)
         layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
         layout.addLayout(bottom_hbox)
         self.setLayout(layout)
 
-        pbar = QProgressBar(self)
-        pbar.setMaximum(1 * 1024 * 1024 * 1024)
+    def update_label(self, clear_push_list=False):
+        if clear_push_list:
+            self.push_list.clear()
+        self.label.setText('\n'.join(op.basename(p) for p in self.push_list))
 
-        def launch_event():
-            while True:
-                global d
-                sr = d.runas("cat ./files/launch.txt")
-                if sr.fail:
-                    continue
-
-                d.runas('touch ./files/key_a')
-
-                # emit
-
-                d.sync.pull('/sdcard/Documents/1G', '1G')
-                d.runas('touch ./files/key_b')
-                time.sleep(1)
-                # def update_ui():
-                #     try:
-                #         s = op.getsize('1G')
-                #         pbar.setValue(s)
-                #         pbar.show()
-                #     except FileNotFoundError:
-                #         ...
-
-                # qthread = QThread(self)
-                # qthread.run = d_pull
-                # qthread.start()
-
-                # while True:
-                #     update_ui()
-
-        thread = QThread(self)
-        thread.run = launch_event
-        thread.start()
+    def paste(self, a0):
+        self.push_list += [u.toLocalFile() for u in a0.mimeData().urls() if u.toLocalFile() not in self.push_list]
+        self.update_label()
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
-            event.accept()
-
-    def dropEvent(self, event):
-        if event.mimeData().hasUrls():
-            self.push_list += [url.toLocalFile() for url in event.mimeData().urls() if
-                               url.toLocalFile() not in self.push_list]
-            self.label.setText('\n'.join(op.basename(p) for p in self.push_list))
-            self.label.setAlignment(Qt.AlignmentFlag.AlignLeft)
-            self.vice_btn.setText('Clear')
-            self.vice_btn.clicked.connect(lambda: ...)
-            self.push_btn.setEnabled(True)
             event.acceptProposedAction()
 
-    # def start_io_task(self):
-    #     thread = QThread()
-    #     thread.run = self.io_task
-    #     thread.start()
-    #
-    #     self.timer = QTimer()
-    #     self.timer.timeout.connect(self.update_ui)
-    #     self.timer.start(1)
-    #
-    # def io_task(self):
-    #     d = adb.device_list()[0]
-    #     d.sync.pull(f'/sdcard/Documents/{file}', file)
-    #     self.timer.stop()
-    #
-    # def update_ui(self):
-    #     try:
-    #         s = op.getsize(file)
-    #         # self.progress_bar.setValue(s)
-    #
-    #     except FileNotFoundError:
-    #         ...
+    def dropEvent(self, event):
+        self.paste(event)
+        event.acceptProposedAction()
 
 
-if __name__ == '__main__':
-    d = Device(adb.device_list()[0])
-    app = QApplication(sys.argv)
-    widget = Widget()
-    widget.show()
-    sys.exit(app.exec())
+app = QApplication(sys.argv)
+push_window = PushWindow()
+push_window.show()
+sys.exit(app.exec())
